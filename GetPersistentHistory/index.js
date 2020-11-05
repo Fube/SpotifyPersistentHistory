cfa8f1a14b2b334515dc6255e4bf9ca2177b090f
Fube <fubeitch@gmail.com>
2020-11-05 13:02:13 -0500

    Added README

--- GetPersistentHistory/index.js
+++ GetPersistentHistory/index.js
@@ -11,12 +11,13 @@ const fs = require('fs');
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const PORT = '8888';
+const PORT = process.env.PORT || '8888';
 const redirect_uri = `https://localhost:${PORT}/callback`;
 const stateKey = 'spotify_auth_state';
 const TIMEOUT = 30*49;
 const uri = `mongodb+srv://${process.env.DB_UNAME}:${process.env.DB_PASS}@spotifypersistenthistory-civb3.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useUnifiedTopology: true });
+
 client.connect();
 const app = express();
 
@@ -25,7 +26,7 @@ const app = express();
  * @param {*[]} songs 
  */
 function ship(songs){
-  if(song.length)
+  if(songs.length)
     client.db('spotify').collection('songs').insertMany(songs)
 }
 
@@ -60,7 +61,7 @@ function renew(){
       if(!error && response.statusCode === 200){
         access_token = body.access_token;
       }else{
-        console.log(err, response.statusCode);
+        console.log(error, response.statusCode);
       }
     });
 }
@@ -103,29 +104,35 @@ app.get('/callback', async function(req, res) {
 
           const pats = [];
           const toAdd = [];
-          for(let item of body.items){
-              const obj = {};
+          for(let { played_at, track } of body.items){
 
-              obj.played_at = item.played_at;
-              pats.push(item.played_at);
-              obj.name = item.track.name;
-              obj.artists = [];
 
-              for(let artist of item.track.artists){
-                  obj.artists.push(artist.name);
-              }
+            const { name, artists, album : { images } , external_urls : { spotify: url } } = track;
 
-              obj.image_url = item.track.album.images[2].url;
-              obj.url = item.track.external_urls.spotify;
+            const obj = {
+              played_at,
+              name,
+              artists: artists.map(n => n.name),
+              image_url: images[2].url,
+              url
+            };
 
-              toAdd.push(obj);
+            pats.push(played_at);
+
+            toAdd.push(obj);
           }
 
           const collection = client.db("spotify").collection("songs");
 
-          const toAvoid = await (await collection.find({played_at: {$in : pats}}).toArray()).map(n => n.played_at);
+          const toAvoid =  (await collection
+                              .find( { played_at: { $in : pats } } )
+                              .project({ _id: 0 })
+                              .toArray())
+                              .map(n => n.played_at);
           ship(toAdd.filter(song => !toAvoid.includes(song.played_at)));
       });
+  
+  
   }
   
   const code = req.query.code || null;
@@ -142,8 +149,8 @@ app.get('/callback', async function(req, res) {
     const authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
-        code: code,
-        redirect_uri: redirect_uri,
+        code,
+        redirect_uri,
         grant_type: 'authorization_code'
       },
       headers: {
@@ -165,6 +172,7 @@ app.get('/callback', async function(req, res) {
         setInterval(renew, 1000 * TIMEOUT / 2);
       }
     });
+    res.redirect('http://localhost:6600')
   }
 });
 
---
+++ README.md
@@ -0,0 +1,28 @@
+# Spotify Persistent History
+## Because Spotify doesn't offer an easy way to see your song history
+
+### How it works
+Spotify actually keeps a list of the last 50 songs you listened. Anything you listened to for 30+ seconds is added to this list<br>
+So every 30\*50 seconds it retrieves your history and adds it to a database (MongoDB)<br>
+It then retrieves those records and displays them to you in your browser<br>
+Note that it deletes records that are 3 months of age (or older)
+
+### Requirments
+This is made by a dev for devs. It is not made to be average user friendly. <br>
+You will need:
+
+- A MongoDB (local or not, I used the free tier of [MongoDB Atlas](https://www.mongodb.com/cloud/atlas))
+- A Spotify dev account
+
+### Set Up
+Make a .env file and add the following fields:
+
+| Field Name     | What it is                        |
+|--------------|------------------------------|
+| DB_UNAME  	 | Your MongoDB username             |
+| DB_PASS		 | Your MongoDB password             |
+| CLIENT_ID	     | Your Spotify dev account's ID     |
+| CLIENT_SECRET  | Your Spotify dev account's secret |
+
+### What it looks like
+![GUI Example](./example.png)
